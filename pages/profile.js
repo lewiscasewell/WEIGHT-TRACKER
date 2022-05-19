2eb934d86311266055c6cc611e61ed98010d06f7
lewiscasewell <email>
2022-05-19 13:12:30 +0100

    fixed invisible date picker in /profile

--- pages/profile.js
+++ pages/profile.js
@@ -206,7 +206,7 @@ export default function Profile() {
                 {isEditBirthDate && (
                   <input
                     type="date"
-                    className="bg-transparent"
+                    className="peer h-10 w-full border-b-2 border-gray-300 bg-transparent text-gray-900 placeholder-transparent focus:border-red-400 focus:outline-none"
                     max={current}
                     value={newBirthDate}
                     placeholder="yyyy-mm-dd"
